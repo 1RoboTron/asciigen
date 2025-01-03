eae6e5c94e605e43cc8f01ae7715dd0eaaa5aa7f
Robo <ashamov2011emil201@gmail.com>
2025-01-03 20:53:50 +0000

    Yes

--- ascii_art.py
+++ ascii_art.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageOps
 
-def image_to_ascii(image_path, output_size, invert=True):  # Изменено на True по умолчанию
+def image_to_ascii(image_path, output_size, invert=True):  
     image = Image.open(image_path)
     image = image.resize(output_size)
     if image.mode == 'RGBA':
@@ -11,5 +11,5 @@ def image_to_ascii(image_path, output_size, invert=True):  # Изменено н
     ascii_chars = "@%#*+=-:.,"
     pixels = image.getdata()
     ascii_str = ''.join(ascii_chars[pixel * len(ascii_chars) // 256] for pixel in pixels)
-    ascii_art = "\n".join(ascii_str[i:i + output_size[0]] for i in range(0, len(ascii_str), output_size[0]))  # Закрывающая скобка добавлена
+    ascii_art = "\n".join(ascii_str[i:i + output_size[0]] for i in range(0, len(ascii_str), output_size[0]))  
     return ascii_art
